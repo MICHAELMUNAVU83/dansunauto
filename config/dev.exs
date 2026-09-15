e035ff602fe94b08b2c50efac42fcc250c5d5cf6
mmunavu <michaelmunavu83@gmail.com>
2026-09-15 16:39:04 +0300

    Set 7551

--- config/dev.exs
+++ config/dev.exs
@@ -19,7 +19,7 @@ config :dansunauto, Dansunauto.Repo,
 # Binding to loopback ipv4 address prevents access from other machines.
 config :dansunauto, DansunautoWeb.Endpoint,
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [port: 7151],
+  http: [port: 7551],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
